c37aeb8b5dcc7ebef00b3dd0a747ca7379458447
Andrew <drewcpete@gmail.com>
2019-10-11 10:45:13 -0700

    I think i'm sick because i threw up.  laid down for a while then came back and flushed out the vendor and order properties

--- Models/Bakery.cs
+++ Models/Bakery.cs
@@ -7,6 +7,28 @@ namespace Bakery.Models
     {
         public string Name {get; set;}
         public string Description {get; set;}
-        public List<Orders> Orders {get; set;}
+        public List<Order> Orders {get; set;}
+
+        public Vendor(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            Orders = new List<Order> ();
+        }
+    }
+
+    public class Order
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public DateTime Date { get; set; }
+
+        public Order(string title, DateTime date)
+        {
+            Title = title;
+            Price = 0;
+            Date = date;
+        }
     }
 }
+
